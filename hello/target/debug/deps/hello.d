9bca6b48ff26e355e2fede19d407df1b91b373a2
tobyatgithub <toby.fangyuan@gmail.com>
2022-12-06 14:00:40 -0800

    first step.

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\Toby Huang\Documents\rust_the_book\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\Toby Huang\Documents\rust_the_book\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
