033f41bd0d24e20affe9b8e7306cc14dacd0d730
tobyatgithub <toby.fangyuan@gmail.com>
2022-12-06 14:33:53 -0800

    add handle connection.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,10 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
+
+        handle_connection(_stream);
         println!("Hello, world!");
     }
+}
+
+fn handle_connection(mut _stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut _stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request)
 }
